ea40b38bcf50879e7e8abf95ea60c5ea06f5689a
Yash Gopal <reach.yashg@gmail.com>
2021-03-01 22:32:45 -0500

    vite tooling + basic page structure

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,24 @@ const postcssImport = require('postcss-import');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss');
+const postcssFontMagician = require('postcss-font-magician');
+const postcssUrl = require('postcss-url');
 const postcssNesting = require('postcss-nesting');
 const postcssCustomProperties = require('postcss-custom-properties');
+const cssnano = require('cssnano');
 
-module.exports = {
+module.exports = (ctx) => ({
   plugins: [
     postcssImport,
     tailwindcss,
     postcssNesting,
     postcssCustomProperties,
-    purgecss({
-      content: ['./**/*.html'],
+    postcssUrl({
+      url: 'copy',
+      assetsPath: 'public/assets/images',
+      useHash: true,
     }),
     autoprefixer,
+    ctx.env === 'production' ? cssnano : false,
   ],
-};
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Brandon Grotesque', 'Helvetica', 'sans-serif'],
+      serif: ['Crimson Text', 'Arial', 'serif'],
+    },
+    colors: {
+      red: {
+        lightest: '#FFEBF1',
+        lighter: '#FD9BBA',
+        light: '#FB0E59',
+        DEFAULT: '#C32B3E',
+        dark: '#A10235',
+        darker: '#50011A',
+      },
+      paper: {
+        light: '#FFF',
+        DEFAULT: '#F8F8F8',
+        dark: '#D6D6D6',
+      }
+    },
     extend: {},
   },
   variants: {
