7ec0c721770120ef503098240c2d27b964ac2c5a
Yash Gopal <reach.yashg@gmail.com>
2021-03-02 13:37:28 -0500

    restructured page. adding tailwind plugins to build

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,29 @@ module.exports = {
       sans: ['Brandon Grotesque', 'Helvetica', 'sans-serif'],
       serif: ['Crimson Text', 'Arial', 'serif'],
     },
-    colors: {
-      red: {
-        lightest: '#FFEBF1',
-        lighter: '#FD9BBA',
-        light: '#FB0E59',
-        DEFAULT: '#C32B3E',
-        dark: '#A10235',
-        darker: '#50011A',
-      },
-      paper: {
-        light: '#FFF',
-        DEFAULT: '#F8F8F8',
-        dark: '#D6D6D6',
+    extend: {
+      colors: {
+        red: {
+          lightest: '#FFEBF1',
+          lighter: '#FD9BBA',
+          light: '#FB0E59',
+          DEFAULT: '#C32B3E',
+          dark: '#A10235',
+          darker: '#50011A',
+        },
+        paper: {
+          light: '#FFF',
+          DEFAULT: '#F8F8F8',
+          dark: '#D6D6D6',
+        }
       }
     },
-    extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
 }
