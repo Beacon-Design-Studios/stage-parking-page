0d12fbd1b2d361635fba26ca832598a27257d37b
Yash Gopal <reach.yashg@gmail.com>
2021-03-12 22:51:12 -0500

    multipage build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+const { resolve } = require('path');
+
 export default {
   root: './src',
   build: {
-    outDir: '../dist'
-  }
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        'main': resolve(__dirname, 'src/index.html'),
+        'privacy-policy': resolve(__dirname, 'src/privacy-policy.html')
+      }
+    }
+  },
 };
